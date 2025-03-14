1182f0ed6e1c98c04b1657535cc9f1547e728535
jrivera17 <jrivera17@myseneca.ca>
2025-03-13 21:32:51 -0400

    fixed import problem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { Row, Col } from 'react-bootstrap';
 
 const geistSans = Geist({
