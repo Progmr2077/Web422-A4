d7df9bbda1a9dac40af2fea635473da810ec90e5
jrivera17 <jrivera17@myseneca.ca>
2025-03-15 23:07:23 -0400

    fixed spacing

--- pages/search.js
+++ pages/search.js
@@ -105,9 +105,9 @@ export default function AdvancedSearch() {
               </div>
               {errors.searchBy && <div className="invalid-feedback">This field is required</div>}
             </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Check type="checkbox" label="Currently on View" {...register('isOnView')} />
-              <Form.Check type="checkbox" label="Highlighted" {...register('isHighlight')} />
+            <Form.Group className="mb-3" style={{ marginTop: '80px' }}>
+            <Form.Check type="checkbox" label="Currently on View" {...register('isOnView')} />
+            <Form.Check type="checkbox" label="Highlighted" {...register('isHighlight')} />
             </Form.Group>
           </Col>
           <Col md={4}>
